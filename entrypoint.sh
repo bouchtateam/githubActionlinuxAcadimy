68c6648a4c0cc7cb32da1c81d3340ef2a79cbc73
bouchtateam <34681030+bouchtateam@users.noreply.github.com>
2020-12-19 19:19:45 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 echo "Hello $INPUT_MYINPUT"
-memory=$(cat  /proc/meminfo | grep "MemTotal")
+# memory=$(cat  /proc/meminfo | grep "MemTotal")
+memory=$(awk '$3=="kB"{$2=$2/1024^2;$3="GB";} 1' /proc/meminfo | column -t)
 echo "::set-output name=memory::$memory"
